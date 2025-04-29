d2fe065c41fb0c0283861464e5503e69e4bea85f
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-04-29 21:28:24 +0700

    Create navigation.js

---
+++ src/_data/navigation.js
@@ -0,0 +1,36 @@
+module.exports = {
+  main: [
+    {
+      title: "Beranda",
+      url: "/"
+    },
+    {
+      title: "Tentang",
+      url: "/#about"
+    },
+    {
+      title: "Program",
+      url: "/#programs"
+    },
+    {
+      title: "Tim",
+      url: "/#team"
+    },
+    {
+      title: "Artikel",
+      url: "/articles/"
+    },
+    {
+      title: "Galeri",
+      url: "/#gallery"
+    },
+    {
+      title: "Timeline",
+      url: "/#timeline"
+    },
+    {
+      title: "Kontak",
+      url: "/#contact"
+    }
+  ]
+};
