2af7be289f8fa9d928852cf320c3d0888ccd7e33
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-04-29 21:17:11 +0700

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,54 @@
+const { DateTime } = require("luxon");
+const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
+module.exports = function(eleventyConfig) {
+  // Copy the `assets` directory to the output
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  
+  // Date formatting
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLLL yyyy");
+  });
+
+  // Minify CSS
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  // Minify HTML output
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if(outputPath && outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  // Custom Markdown library setup
+  let markdownLibrary = markdownIt({
+    html: true,
+    breaks: true,
+    linkify: true
+  }).use(markdownItAnchor);
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true
+  };
+};
