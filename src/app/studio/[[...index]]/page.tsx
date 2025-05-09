dc5ff2e23ec44a9496a5c5903ff60a7b284e4646
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-10 01:15:27 +0700

    Update page.tsx

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { Studio } from 'sanity'
+import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
 export default function StudioPage() {
-  return <Studio config={config} />
+  return <NextStudio config={config} />
 }
