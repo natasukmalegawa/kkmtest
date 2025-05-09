7d879e6c57f117276e3f361487081f695a84848e
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-09 22:52:50 +0700

    Create hero.ts

---
+++ sanity/schemas/hero.ts
@@ -0,0 +1,30 @@
+export default {
+  name: 'hero',
+  title: 'Hero Section',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'subtitle',
+      title: 'Subtitle',
+      type: 'text',
+    },
+    {
+      name: 'ctaText',
+      title: 'Call to Action Text',
+      type: 'string',
+    },
+    {
+      name: 'backgroundImage',
+      title: 'Background Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+}
