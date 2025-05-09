22bd651079e63fec07473ee1a5d6dcb57ac21363
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-09 22:35:47 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['cdn.sanity.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
