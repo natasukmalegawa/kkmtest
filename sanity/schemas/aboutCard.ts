5c2bfec0439dc0dea99f63532cfaeac75c1f261e
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-09 22:53:40 +0700

    Create aboutCard.ts

---
+++ sanity/schemas/aboutCard.ts
@@ -0,0 +1,31 @@
+export default {
+  name: 'aboutCard',
+  title: 'About Card',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'icon',
+      title: 'Icon',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'order',
+      title: 'Order',
+      type: 'number',
+      description: 'Order of the card',
+    },
+  ],
+}
