75de31e5c20883690f97a102ad914cf0cc297f85
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-09 22:55:54 +0700

    Create footerColumn.ts

---
+++ sanity/schemas/footerColumn.ts
@@ -0,0 +1,40 @@
+export default {
+  name: 'footerColumn',
+  title: 'Footer Column',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'links',
+      title: 'Links',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'title',
+              title: 'Title',
+              type: 'string',
+            },
+            {
+              name: 'url',
+              title: 'URL',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'order',
+      title: 'Order',
+      type: 'number',
+      description: 'Order of the footer column',
+    },
+  ],
+}
