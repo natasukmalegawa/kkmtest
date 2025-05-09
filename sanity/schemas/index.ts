fe06d9faac8598844896dc3b3a9807fd462f7f88
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-09 23:45:49 +0700

    Create index.ts

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,17 @@
+import siteSettings from './siteSettings'
+import navigation from './navigation'
+import hero from './hero'
+import aboutCard from './aboutCard'
+import program from './program'
+import teamMember from './teamMember'
+import footerColumn from './footerColumn'
+
+export const schemaTypes = [
+  siteSettings,
+  navigation,
+  hero,
+  aboutCard,
+  program,
+  teamMember,
+  footerColumn,
+]
