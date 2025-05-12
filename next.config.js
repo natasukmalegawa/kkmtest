ba789553b9331479a02c8f22969b7334d4b4bdb9
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-12 08:20:47 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
         port: '',
-        pathname: '/images/**',
+        pathname: '/images/*/**',
       },
     ],
   },
