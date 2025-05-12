0453df5e41108bded9cc052e9ae233d26cf984de
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-12 10:08:35 +0700

    Update route.ts

--- src/app/api/exit-draft/route.ts
+++ src/app/api/exit-draft/route.ts
@@ -2,6 +2,11 @@ import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export async function GET() {
-  draftMode().disable()
+  // Tunggu promise dari draftMode() selesai
+  const draft = draftMode()
+  
+  // Kemudian panggil method disable pada objek yang dikembalikan
+  draft.disable()
+  
   redirect('/')
 }
